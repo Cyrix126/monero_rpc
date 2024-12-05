4cd537a8c6097a146ee393720d93649e89d4e3c6
sneurlax <sneurlax@gmail.com>
2024-12-05 17:05:59 -0600

    fix: fix get_outs example

--- example/monero_rpc_example.dart
+++ example/monero_rpc_example.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:monero_rpc/src/daemon_rpc.dart';
 
 void main() async {
@@ -12,16 +14,31 @@ void main() async {
     final infoResult = await daemonRpc.call('get_info', {});
     print('get_info response:');
     print(infoResult);
+    print("\n\n\n");
 
-    // Call get_transactions via direct endpoint.
-    final txsResult = await daemonRpc.postToEndpoint('/get_transactions', {
-      'txs_hashes': [
-        'd6e48158472848e6687173a91ae6eebfa3e1d778e65252ee99d7515d63090408'
-      ],
-      'decode_as_json': true,
-    });
-    print('get_transactions response:');
-    print(txsResult);
+    // // Call get_transactions via direct endpoint.
+    // final txsResult = await daemonRpc.postToEndpoint('/get_transactions', {
+    //   'txs_hashes': [
+    //     'd6e48158472848e6687173a91ae6eebfa3e1d778e65252ee99d7515d63090408'
+    //   ],
+    //   'decode_as_json': true,
+    // });
+    // print('get_transactions response:');
+    // print(txsResult);
+    // print("\n\n\n");
+
+    // Call get_outs via direct endpoint.
+    final outsResponse = await daemonRpc.postToEndpoint(
+      '/get_outs',
+      {
+        'get_txid': true,
+        'outputs': [
+          {'index': 5164903},
+        ],
+      },
+    );
+    print('get_outs response:');
+    print(jsonEncode(outsResponse));
   } catch (e) {
     print('Error: $e');
   }
