4c730f251ac06c08358409b8df65d2cb7cbf39d1
Louis-Marie Baer <lm@baermail.fr>
2024-12-21 22:10:01 +0100

    feat: add client property to DaemonRpc class

--- test/daemon_rpc_test.dart
+++ test/daemon_rpc_test.dart
@@ -1,9 +1,11 @@
+import 'package:http/http.dart';
 import 'package:monero_rpc/src/daemon_rpc.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('WalletRpc call method', () async {
     final walletRpc = DaemonRpc(
+      Client(),
       'http://localhost:18081/json_rpc',
       username: 'user',
       password: 'password',
